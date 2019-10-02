1bb2c66e2464e821a25504991e033ff373909ec1
at-loantruong <loan.truong@asiantech.vn>
2019-10-02 17:02:04 +0700

    Add and delete todo task

--- vue-pwa-todolist/src/components/TodoList.vue
+++ vue-pwa-todolist/src/components/TodoList.vue
@@ -1,29 +1,75 @@
 <template>
   <div class="page-todo">
-    <Header/>
+    <Header />
     <main class="page-main">
       <div class="container">
-        <Form/>
-        <List/>
+        <todo-form @addTodo="addTodo"></todo-form>
+        <div class="todo-list-section">
+          <todo-item
+            v-for="(todo, index) in todos"
+            :key="todo.id"
+            :todo="todo"
+            :index="index"
+            :completed="todo.completed"
+            @removeTodo="removeTodo"
+            @changedCompleted="changedCompleted">
+          </todo-item>
+        </div>
       </div>
     </main>
-    <Footer/>
+    <Footer />
   </div>
 </template>
-<script lang="ts">
-  import Vue from 'vue';
-  import Header from './layouts/Header.vue';
-  import Footer from './layouts/Footer.vue';
-  import Form from './features/Form.vue';
-  import List from './features/List.vue';
+<script lang='ts'>
+import Vue from 'vue';
+import Header from './layouts/Header.vue';
+import Footer from './layouts/Footer.vue';
+import TodoForm from './features/TodoForm.vue';
+import TodoItem from './features/TodoItem.vue';
 
-  export default ({
-    name: 'TodoApp',
-    components: {
-      Header,
-      Footer,
-      Form,
-      List,
+export default {
+  name: 'TodoApp',
+  components: {
+    Header,
+    Footer,
+    TodoForm,
+    TodoItem,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'title',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'title',
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo(todo: any) {
+      if (todo.trim() === '') {
+        return;
+      }
+      this.todos.unshift({
+        id: Math.random(),
+        title: todo,
+        completed: false,
+      });
     },
-  });
+    removeTodo(todo: any) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    changedCompleted(todo: any) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1, todo);
+    },
+  },
+};
 </script>
