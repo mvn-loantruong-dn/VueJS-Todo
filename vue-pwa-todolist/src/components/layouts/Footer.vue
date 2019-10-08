38c704fdb91363c80f883d0a18d32b3ced9898c5
at-loantruong <loan.truong@asiantech.vn>
2019-10-08 17:22:39 +0700

    Remove unused code

--- vue-pwa-todolist/src/components/layouts/Footer.vue
+++ vue-pwa-todolist/src/components/layouts/Footer.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="page-footer">
-    <!-- <div class="container"> -->
-      <ul class="todos-footer-filter">
-        <li class="filter-item" @click="filterTodos('all')" :class="{active:visibility == 'all'}">
-          <i class="icon-all"></i>
-          <span class="filter-text">All Tasks</span>
-        </li>
-        <li class="filter-item" @click="filterTodos('active')" :class="{active:visibility == 'active'}">
-          <i class="icon-planning"></i>
-          <span class="filter-text">Active</span>
-        </li>
-        <li class="filter-item" @click="filterTodos('complete')" :class="{active:visibility == 'complete'}">
-          <i class="icon-completed"></i>
-          <span class="filter-text">Completed</span>
-        </li>
-        <li class="filter-item" @click="removeCompleted('clear')" :class="{active:visibility == 'clear', disabled:!completedTodo}">
-          <i class="icon-remove"></i>
-          <span class="filter-text">Clear completed</span>
-        </li>
-        <li class="slide"></li>
-      </ul>
-    <!-- </div> -->
+    <ul class="todos-footer-filter">
+      <li class="filter-item" @click="filterTodos('all')" :class="{active:visibility == 'all'}">
+        <i class="icon-all"></i>
+        <span class="filter-text">All Tasks</span>
+      </li>
+      <li class="filter-item" @click="filterTodos('active')" :class="{active:visibility == 'active'}">
+        <i class="icon-planning"></i>
+        <span class="filter-text">Active</span>
+      </li>
+      <li class="filter-item" @click="filterTodos('complete')" :class="{active:visibility == 'complete'}">
+        <i class="icon-completed"></i>
+        <span class="filter-text">Completed</span>
+      </li>
+      <li class="filter-item" @click="removeCompleted('clear')" :class="{active:visibility == 'clear', disabled:!completedTodo}">
+        <i class="icon-remove"></i>
+        <span class="filter-text">Clear completed</span>
+      </li>
+      <li class="slide"></li>
+    </ul>
   </div>
 </template>
 <script lang="ts">
