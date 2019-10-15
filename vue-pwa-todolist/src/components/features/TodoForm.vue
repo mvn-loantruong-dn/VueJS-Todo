729cc6522cdc6190ca0f3ebafed2632b0e423c2d
at-loantruong <loan.truong@asiantech.vn>
2019-10-15 13:25:44 +0700

    Fix font

--- vue-pwa-todolist/src/components/features/TodoForm.vue
+++ vue-pwa-todolist/src/components/features/TodoForm.vue
@@ -2,7 +2,7 @@
   <div class="form-section">
     <form class="form-add-task" @submit.prevent="addTodo()">
       <div class="form-group">
-        <input type="text" v-model="newTodo" maxlength="35" class="form-input" placeholder="Add a task..."/>
+        <input type="text" v-model="newTodo" maxlength="30" class="form-input" placeholder="Add a task..."/>
         <button type="submit" class="input-group-addon" @click="addTodo()"><i class="icon-add"></i></button>
       </div>
     </form>
