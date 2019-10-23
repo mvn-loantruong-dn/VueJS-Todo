a06dffb46a2557944c68a7cbdbd6dd22ae26c881
at-loantruong <loan.truong@asiantech.vn>
2019-10-23 11:52:31 +0700

    Deploy firebase

--- vue-pwa-todolist/src/main.ts
+++ vue-pwa-todolist/src/main.ts
@@ -2,9 +2,21 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyBCJrK65yax-9IfyUSB5iQc9BsyOkS5OSQ',
+  authDomain: 'vue-todo-2c282.firebaseapp.com',
+  databaseURL: 'https://vue-todo-2c282.firebaseio.com',
+  projectId: 'vue-todo-2c282',
+  storageBucket: 'vue-todo-2c282.appspot.com',
+  messagingSenderId: '561118336749',
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
